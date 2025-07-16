5f6857ccf8715dfe44d538eee9e0c6a44645e8a1
Jackuri <leedg92@naver.com>
2025-07-17 07:30:43 +0900

    두번째 기능 완료(ELK+Airflow)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './components/layout/Layout';
 import BoardList from './pages/board/List';
 import BoardDetail from './pages/board/Detail';
 import BoardWrite from './pages/board/Write';
+import NewsLearning from './pages/news-learning/NewsLearning';
 import Home from './pages/Home';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Route path="/board" element={<BoardList />} />
             <Route path="/board/write" element={<BoardWrite />} />
             <Route path="/board/:id" element={<BoardDetail />} />
+            <Route path="/news-learning" element={<NewsLearning />} />
           </Routes>
         </div>
       </Layout>
---
+++ src/pages/news-learning/NewsLearning.tsx
@@ -0,0 +1,9 @@
+const NewsLearning: React.FC = () => {
+    return (
+        <div style={{ maxWidth: '1200px', height: '100vh' }}>
+            <iframe src="http://localhost:8010/app/dashboards#/view/6a6e1eba-6bd9-4f7a-a713-38d98e55c004?embed=true&_g=(refreshInterval:(pause:!t,value:60000),time:(from:now-3600d,to:now))&_a=()&show-query-input=true&hide-filter-bar=true" style={{ height: '100%', width: '100%', border: 'none' }}></iframe>
+        </div>
+    );
+};
+
+export default NewsLearning;
